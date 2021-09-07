35fbaa7b941386ba87f7c1f2aa48a1af1fb2c850
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2021-09-07 15:51:16 +0900

    #1 複数のカテゴリの判定をJS側に移設しました。

--- mani-estate-map.php
+++ mani-estate-map.php
@@ -13,141 +13,121 @@ $mani_estate_map = Mani_Estate_Map::instance();
 
 class Mani_Estate_Map {
 
-  private static $instance;
+    private static $instance;
 
-  public static function instance() {
-    if ( self::$instance == null ) {
-      self::$instance = new self();
-    }
-    return self::$instance;
-  }
-
-  public function activate() {
-  }
-
-  public function deactivate() {
-  }
-
-  public function __construct() {
-    register_activation_hook (__FILE__, array($this, 'activate'));
-    register_deactivation_hook (__FILE__, array($this, 'deactivate'));
-    add_action('init', array($this, 'create_estate_post_type'));
-  }
-
-  public function create_estate_post_type() {
-    register_post_type(
-      'estate',
-      array(
-        'label' => '自社所有物件',
-        'public' => true,
-        'has_archive' => true,
-        'menu_position' => 4,
-        'supports' => array(
-          'title',
-          'editor',
-          'author',
-          'thumbnail',
-          'excerpt',
-          'custom-fields',
-          'revisions'
-        ),
-        'taxonomies' => array('estate_cat'),
-        'show_in_rest' => true
-      )
-    );
-
-    register_taxonomy(
-      'estate_cat',
-      'estate',
-      array(
-        'label' => '物件カテゴリー',
-        'labels' => array(
-          'popular_items' => 'よく使う物件カテゴリー',
-          'edit_item' =>'物件カテゴリーを編集',
-          'add_new_item' => '物件カテゴリーを追加',
-          'search_items' => '物件カテゴリーを検索'
-        ),
-        'public' => true,
-        'description' => '物件カテゴリーの説明文です。',
-        'hierarchical' => true,
-        'show_in_rest' => true
-      )
-    );
-  }
-
-  public function mani_estate_query($category = array()) {
-
-    $args = array(
-      'post_type'      => 'estate',
-      'posts_per_page' => -1,
-    );
-
-    $search = array();
-    if ( !in_array( 'sold', $category, true ) ) {
-      $search[] = array(
-        'taxonomy' => 'estate_cat',
-        'terms' => array('sold'),
-        'field' => 'slug',
-        'operator' => 'NOT IN',
-      );
-    } else {
-      $category = array_diff($category, array('sold'));
-      $category = array_values($category);
+    public static function instance() {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
 
-    $esttype = array();
-    if ( in_array( 'house', $category, true ) ) {
-      $esttype[] = 'house';
-    }
-    if ( in_array( 'apartment', $category, true ) ) {
-      $esttype[] = 'apartment';
-    }
-    if ( count( $esttype ) > 0 ) {
-      $search[] = array(
-        'taxonomy' => 'estate_cat',
-        'terms' => $esttype,
-        'field' => 'slug',
-        'operator' => 'IN',
-      );
+    public function activate() {
     }
 
-    if ( in_array( 'solar', $category, true ) ) {
-      $search[] = array(
-        'taxonomy' => 'estate_cat',
-        'terms' => 'solar',
-        'field' => 'slug',
-      );
+    public function deactivate() {
     }
 
-    if ( count($search) > 0 ) {
-      $search[] = array('relation' => 'AND');
-      $args = array(
-        'post_type' => 'estate',
-        'posts_per_page' => -1,
-        'tax_query' => $search,
-      );
+    public function __construct() {
+        register_activation_hook(__FILE__, array($this, 'activate'));
+        register_deactivation_hook(__FILE__, array($this, 'deactivate'));
+        add_action('init', array($this, 'create_estate_post_type'));
     }
 
-    return $args;
-  }
+    public function create_estate_post_type() {
+        register_post_type(
+            'estate',
+            array(
+                'label' => '自社所有物件',
+                'public' => true,
+                'has_archive' => true,
+                'menu_position' => 4,
+                'supports' => array(
+                    'title',
+                    'editor',
+                    'author',
+                    'thumbnail',
+                    'excerpt',
+                    'custom-fields',
+                    'revisions'
+                  ),
+                'taxonomies' => array('estate_cat'),
+                'show_in_rest' => true
+            )
+        );
 
-  public function get_map_icon($slugs = array()) {
-    $icon = '';
-    if (count($slugs) == 1 && $slugs[0] == 'solar') {
-      return 'solar';
+        register_taxonomy(
+            'estate_cat',
+            'estate',
+            array(
+              'label' => '物件カテゴリー',
+              'labels' => array(
+                'popular_items' => 'よく使う物件カテゴリー',
+                'edit_item' =>'物件カテゴリーを編集',
+                'add_new_item' => '物件カテゴリーを追加',
+                'search_items' => '物件カテゴリーを検索'
+              ),
+              'public' => true,
+              'description' => '物件カテゴリーの説明文です。',
+              'hierarchical' => true,
+              'show_in_rest' => true
+            )
+        );
     }
-    if ( in_array( 'house', $slugs, true ) ) {
-      $icon = 'house';
-      if ( in_array( 'solar', $slugs, true ) ) {
-        $icon .= '_solar';
-      }
+
+    public function mani_estate_query($category = array()) {
+
+        $args = array(
+          'post_type'      => 'estate',
+          'posts_per_page' => -1,
+        );
+
+        $search = array();
+        if (!in_array('sold', $category, true)) {
+            $search[] = array(
+            'taxonomy' => 'estate_cat',
+            'terms' => array('sold'),
+            'field' => 'slug',
+            'operator' => 'NOT IN',
+            );
+        } else {
+            $category = array_diff($category, array('sold'));
+            $category = array_values($category);
+        }
+
+        $esttype = array();
+        if (in_array('house', $category, true)) {
+            $esttype[] = 'house';
+        }
+        if (in_array('apartment', $category, true)) {
+            $esttype[] = 'apartment';
+        }
+        if (count($esttype) > 0) {
+            $search[] = array(
+            'taxonomy' => 'estate_cat',
+            'terms' => $esttype,
+            'field' => 'slug',
+            'operator' => 'IN',
+            );
+        }
+
+        if (in_array('solar', $category, true)) {
+            $search[] = array(
+            'taxonomy' => 'estate_cat',
+            'terms' => 'solar',
+            'field' => 'slug',
+            );
+        }
+
+        if (count($search) > 0) {
+            $search[] = array('relation' => 'AND');
+            $args = array(
+              'post_type' => 'estate',
+              'posts_per_page' => -1,
+              'tax_query' => $search,
+            );
+        }
+
+        return $args;
     }
-    if ( in_array( 'apartment', $slugs, true ) ) {
-      $icon = 'apartment';
-      if ( in_array( 'solar', $slugs, true ) ) {
-        $icon .= '_solar';
-      }
-    }
-    return $icon;
-  }
 }
